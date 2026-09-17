14d5ece9bee0490f6081e2b043f7587ed0985761
G-ENC <bartugenc@gmail.com>
2026-09-17 14:17:18 +0300

    started moves

--- cosntants.py
+++ cosntants.py
@@ -76,6 +76,16 @@ class Coordinate(IntEnum):
     g1 = 62
     h1 = 63
 
+class Color(IntEnum):
+    WHITE = 0
+    BLACK = 1
+
+    def __invert__(self):
+        if self == Color.WHITE:
+            return Color.BLACK
+        else: 
+            return Color.WHITE
+
 bitboard_squares = ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8', 
                     'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 
                     'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6', 
