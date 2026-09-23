ab9c9ff5b53b9c0d7f26cf789531e5707eb670a4
G-ENC <bartugenc@gmail.com>
2026-09-23 08:09:36 +0300

    board squares done

--- game.py
+++ game.py
@@ -5,12 +5,13 @@ import pygame
 
 class Game:
 
-  def __init__(self,screen_w, screen_h):
+  def __init__(self,screen_w=800, screen_h=800):
+
 
     self.width = screen_w
     self.height = screen_h
 
-    self.running = True
+    self.run = True
 
     pygame.init()
 
@@ -20,23 +21,29 @@ class Game:
 
 
 
-  def drawBoard(self):
-    for row in range(8):
-      for column in range(8):
-        pygame.draw.rect(self.screen, (255,0,0), )
-        pygame.
+  def drawCheckerBoardPattern(self, n=8):
+    cell_w = self.width/n
+    cell_h = self.height/n
+
+    white = True
+    for column in range(n):
+      white = not white
+      for row in range(n):
+        sq_rect = pygame.Rect(cell_w*row, cell_h*column, cell_w, cell_h)
+        if white:
+          pygame.draw.rect(self.screen, (0,0,0), sq_rect)
+        else:
+          pygame.draw.rect(self.screen, (200,0,200), sq_rect)
+        white = not white
 
   def initGame(self):
 
-    run = True
+    self.drawCheckerBoardPattern()
 
-    while run:
-
-      
-
+    while self.run:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
-          run = False
+          self.run = False
 
       pygame.display.update()
     pygame.quit()
